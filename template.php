8476c3537cd241533ba38e22ca4264e7932aa7fb
ThanGrove <than@virginia.edu>
2015-06-16 16:13:41 -0400

    Adding js and updating templates

--- template.php
+++ template.php
@@ -9,3 +9,6 @@
   *   This is the template.php file for a child sub-theme of the Shanti Sarvaka theme.
   *   Use it to implement custom functions or override existing functions in the theme. 
   */ 
+function sarvaka_images_preprocess_views_view(&$vars) {
+	dpm($vars, 'vars in view preprocess');
+}
