f6a2b9979df564c79de0e3466ae088e5ada4aa56
ThanGrove <than@virginia.edu>
2015-06-26 09:23:39 -0400

    updating grid-init in an attempt to get it to work.

--- js/contrib/grid-init.js
+++ js/contrib/grid-init.js
@@ -4,18 +4,18 @@
  */
 (function ($) {
 	
-$(document).ready(function(){
-  /* Start rowGrid.js */
-	$(".container.og-grid").rowGrid({itemSelector: ".item", minMargin: 10, maxMargin: 10, firstItemClass: "first-item"});
-	
-	Grid.init();
-	
-	var itemHeight = $(this).closest('.item').height();	    
-	$('.og-grid .item > a').children('img').attr('height', itemHeight );    
-	  
-	  // --- Than - we need some form of the following for resize
-	  // $(window).bind('load resize orientationchange', galleryGrid_Refresh );  
-	  
+	$(document).ready(function(){
+	  /* Start rowGrid.js */
+		$(".container.og-grid").rowGrid({itemSelector: ".item", minMargin: 10, maxMargin: 10, firstItemClass: "first-item"});
+		
+		Grid.init();
+		
+		var itemHeight = $(this).closest('.item').height();	    
+		$('.og-grid .item > a').children('img').attr('height', itemHeight );    
+		  
+		  // --- Than - we need some form of the following for resize
+		  $(window).bind('load resize orientationchange', galleryGrid_Refresh );  
+		  
 	});
 	  
 }) (jQuery);
