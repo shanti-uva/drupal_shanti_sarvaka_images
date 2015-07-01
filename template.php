a6e6298250e33bf26cd8f2bbeeccd7c16356478c
ThanGrove <than@virginia.edu>
2015-07-01 09:22:47 -0400

    adding file url link to details popup

--- template.php
+++ template.php
@@ -40,8 +40,9 @@ function sarvaka_images_preprocess_views_view(&$vars) {
 				$fdesc = substr($fdesc, 0, 500);
 				$fdesc = substr($fdesc, 0, strrpos($fdesc, ' ')) . "...";
 			}
+			$furl = url('file/' . $ftitle);
 			$rows .= '<div class="item">
-		    <a href="#" data-largesrc="' . $large_path . '" data-title="' . $ftitle . '" data-description="' . $fdesc . '">
+		    <a href="' . $furl . '" data-largesrc="' . $large_path . '" data-title="' . $ftitle . '" data-description="' . $fdesc . '">
 	        <img src="' . $thumb_path . '" >
 		    </a>
 	    </div>';
