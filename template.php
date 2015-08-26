22d65a980b4ccff273c77c720f8e33b5e26dc70a
ThanGrove <than@virginia.edu>
2015-08-26 12:37:41 -0400

    changing position of grid.js and jquery.row-grid.js in relation to theme js.

--- template.php
+++ template.php
@@ -18,9 +18,9 @@
 function sarvaka_images_preprocess_views_view(&$vars) {
 	//dpm($vars, 'vars in view preprocess 2');
 	if ($vars['view']->name == 'media_sharedshelf_my_images') {
-		drupal_add_js(drupal_get_path('theme', 'sarvaka_images') . '/js/contrib/jquery.row-grid.js', array('group'=>JS_THEME, 'weight'=>9970));
-		drupal_add_js(drupal_get_path('theme', 'sarvaka_images') . '/js/contrib/grid-init.js', array('group'=>JS_THEME, 'weight'=>9980));
-		drupal_add_js(drupal_get_path('theme', 'sarvaka_images') . '/js/contrib/grid.js', array('group'=>JS_THEME, 'weight'=>9990));
+		drupal_add_js(drupal_get_path('theme', 'sarvaka_images') . '/js/contrib/jquery.row-grid.js', array('group'=>JS_LIBRARY, 'weight'=>9970));
+		//drupal_add_js(drupal_get_path('theme', 'sarvaka_images') . '/js/contrib/grid-init.js', array('group'=>JS_LIBRARY, 'weight'=>9980));
+		drupal_add_js(drupal_get_path('theme', 'sarvaka_images') . '/js/contrib/grid.js', array('group'=>JS_LIBRARY, 'weight'=>9990));
 		drupal_add_css(drupal_get_path('theme', 'sarvaka_images') . '/css/grid-components.css');
 		$view = $vars['view'];
 		$results = $view->result;
