2bc6bf6d96086e62770b5449cc34bdf41c636237
dukarla <artesano@vpn-user-1-40.itc.Virginia.EDU>
2015-08-27 16:45:29 -0500

    testing scroll position
    would like to expose a little of the thumbs below the popup

--- js/contrib/grid.js
+++ js/contrib/grid.js
@@ -179,7 +179,7 @@ var Grid = (function($) {
 		// used to know if the preview will expand in a different row
 		previewPos = -1,
 		// extra amount of pixels to scroll the window
-		scrollExtra = 0,
+		scrollExtra = -50,
 		// extra margin when expanded (between preview overlay and the next items)
 		marginExpanded = 10,
 		$window = $( window ), winsize,
