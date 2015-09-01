e7f5dc35646f4de09b01f52061a2cf5094f2875b
ThanGrove <than@virginia.edu>
2015-09-01 12:18:49 -0400

    Adding pdf templmate

--- template.php
+++ template.php
@@ -27,7 +27,8 @@ function sarvaka_images_preprocess_views_view(&$vars) {
 		$rows = '<div id="og-grid" class="container og-grid">';
 		foreach ($results as $res) {
 			$file = file_load($res->fid);
-			$furi = str_replace('sharedshelf://', 'public://media-sharedshelf/', check_plain($file->uri)) . sarvaka_images_get_image_extension($file);
+			$file_ext = ($file->type == 'document') ? '.jpg' : sarvaka_images_get_image_extension($file);
+			$furi = str_replace('sharedshelf://', 'public://media-sharedshelf/', check_plain($file->uri)) . $file_ext;
 			$thumb_path = image_style_url('media_thumbnail', $furi) ;
 			$large_path = image_style_url('large', $furi) ;
 	
