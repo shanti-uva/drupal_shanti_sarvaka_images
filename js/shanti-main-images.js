acffebff0c64a57ea892e60d22811867f604e2c7
ThanGrove <than@virginia.edu>
2015-09-04 15:41:25 -0400

    Updating call to grid initialization to ensure images are loaded before it is done (MANU-1872)

--- js/shanti-main-images.js
+++ js/shanti-main-images.js
@@ -1,14 +1,21 @@
+(function ($) {
 Drupal.behaviors.shantiImages = {
   attach: function (context, settings) {
   	if(context == document) {
-  		jQuery(".container.og-grid").rowGrid({itemSelector: ".item", minMargin: 5, maxMargin: 5, firstItemClass: "first-item"});
-		
-			Grid.init();
-			
-			// What do these two lines do? May be the problem with images not showing up (MANU-1872) (ndg)
-			//var itemHeight = jQuery(this).closest(".item").height();	    
-			//jQuery(".og-grid .item > a").children("img").attr("height", itemHeight );    
-			  
+  		
+			var $grid = $( '#og-grid' );
+  		$grid.imagesLoaded( function() {
+	  		$(".container.og-grid").rowGrid({itemSelector: ".item", minMargin: 5, maxMargin: 5, firstItemClass: "first-item"});
+				Grid.init();
+				
+  		}); 
+  		
+				// What do these two lines do? "this" does not have a clear referent here
+				// May be the problem with images not showing up (MANU-1872) (ndg)
+				var itemHeight = jQuery(this).closest(".item").height();	    
+				jQuery(".og-grid .item > a").children("img").attr("height", itemHeight );  
+				
   	}
   }
 };
+}) (jQuery);
