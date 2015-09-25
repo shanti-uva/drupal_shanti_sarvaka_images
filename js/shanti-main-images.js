da8aec67c2935e8f9dbca889bf17a6d917decd9a
ThanGrove <than@virginia.edu>
2015-09-25 07:03:07 -0400

    adding file--image template and updating js to not try flex images unless there's a grid

--- js/shanti-main-images.js
+++ js/shanti-main-images.js
@@ -4,26 +4,29 @@
 	  	if(context == document) {
 	  		
 				var $grid = $( '#og-grid' );
-	  		$grid.imagesLoaded( function() {
-	  			$('#og-grid .item ').each(function() {
-	  				var img = $(this).find('img');
-	  				var imgobj = new Image();
-	  				imgobj.src = img.attr('src');
-	  				$(this).attr('data-w', imgobj.width);
-	  				$(this).attr('data-h', imgobj.height);
-	  			});
-	  			new flexImages({ selector: '#og-grid', rowHeight: 180 });
-	  			/*
-		  		$(".container.og-grid").rowGrid({itemSelector: ".item", minMargin: 5, maxMargin: 5, firstItemClass: "first-item"});
-					Grid.init();
-					*/
-	  		}); 
+				if ($grid.length > 0) {
+		  		$grid.imagesLoaded( function() {
+		  			$('#og-grid .item ').each(function() {
+		  				var img = $(this).find('img');
+		  				var imgobj = new Image();
+		  				imgobj.src = img.attr('src');
+		  				$(this).attr('data-w', imgobj.width);
+		  				$(this).attr('data-h', imgobj.height);
+		  			});
+		  			if (typeof(flexImages) == "function") {
+			  			new flexImages({ selector: '#og-grid', rowHeight: 180 });
+			  		}
+		  			/*
+			  		$(".container.og-grid").rowGrid({itemSelector: ".item", minMargin: 5, maxMargin: 5, firstItemClass: "first-item"});
+						Grid.init();
+						*/
+		  		}); 
 	  		
 					// What do these two lines do? "this" does not have a clear referent here
 					// May be the problem with images not showing up (MANU-1872) (ndg)
 					var itemHeight = jQuery(this).closest(".item").height();	    
 					jQuery(".og-grid .item > a").children("img").attr("height", itemHeight );  
-					
+				}
 	  	}
 	  }
 	};
