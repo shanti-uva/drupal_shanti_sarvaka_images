7ae49eeab539ccbba06d21fa2d579870c68eef57
dukarla <artesano@vpn-user-1-245.itc.Virginia.EDU>
2015-09-30 13:28:17 -0600

    minor improve target on gallery

--- js/contrib/grid.js
+++ js/contrib/grid.js
@@ -170,7 +170,7 @@ $.fn.imagesLoaded = function( callback ) {
 var Grid = (function($) {
 	
 		// list of items
-	var $grid = $( '.og-grid' ),
+	var $grid = $( '#og-grid' ),
 		// the items
 		$items = $grid.children( '.item' ),
 		// current expanded item's index
