e57b79df85c43613e98394717b0047bb2de1b1f6
ThanGrove <than@virginia.edu>
2015-10-15 17:36:52 -0400

    removing height adjustment that possibly creates 0 height

--- js/shanti-main-images.js
+++ js/shanti-main-images.js
@@ -5,8 +5,8 @@
 				if ($("#og-grid").length > 0) {
 					$("#og-grid").rowGrid({itemSelector: ".item", minMargin: 10, maxMargin: 10, firstItemClass: "first-item"});
 					Grid.init();
-					var itemHeight = jQuery(this).closest(".item").height();	    
-					$("#og-grid .item > a").children("img").attr("height", itemHeight );
+					//var itemHeight = jQuery(this).closest(".item").height();	    
+					//$("#og-grid .item > a").children("img").attr("height", itemHeight );
 				}
 			}
 	    }
