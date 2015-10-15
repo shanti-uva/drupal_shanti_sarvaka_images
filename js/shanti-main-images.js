4a2327583c7fe5c87e6fb7c14ace938b8f3a2ec0
ThanGrove <than@virginia.edu>
2015-10-15 17:49:58 -0400

    reverting height setting code.

--- js/shanti-main-images.js
+++ js/shanti-main-images.js
@@ -5,11 +5,8 @@
 				if ($("#og-grid").length > 0) {
 					$("#og-grid").rowGrid({itemSelector: ".item", minMargin: 10, maxMargin: 10, firstItemClass: "first-item"});
 					Grid.init();
-					//var itemHeight = jQuery(this).closest(".item").height();	    
-					setTimeout(function() {
-						var itemHeight = jQuery("#og-grid .item").eq(0).height();
-						$("#og-grid .item > a").children("img").attr("height", itemHeight );
-					}, 500);
+					var itemHeight = jQuery(this).closest(".item").height();	    
+					$("#og-grid .item > a").children("img").attr("height", itemHeight );
 				}
 			}
 	    }
