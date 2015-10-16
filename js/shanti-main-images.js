10308d4ec4d9891d4e635da06a38179ecbeeca4c
ThanGrove <than@virginia.edu>
2015-10-16 11:19:34 -0400

    clearing preset widths and heights and delaying grid init with timeout

--- js/shanti-main-images.js
+++ js/shanti-main-images.js
@@ -4,9 +4,13 @@
 			if(context == document) {
 				if ($("#og-grid").length > 0) {
 					$("#og-grid").rowGrid({itemSelector: ".item", minMargin: 10, maxMargin: 10, firstItemClass: "first-item"});
-					Grid.init();
-					var itemHeight = jQuery(this).closest(".item").height();	 
-					$("#og-grid .item > a").children("img").attr("height", itemHeight );
+					setTimeout( $.proxy(function() {
+						$('#og-grid items').attr('style', '');
+						$('#og-grid items img').attr('width', '');
+						Grid.init();
+						var itemHeight = jQuery(this).closest(".item").height();	 
+						$("#og-grid .item > a").children("img").attr("height", itemHeight );
+					}, this), 1000);
 				}
 			}
 	    }
