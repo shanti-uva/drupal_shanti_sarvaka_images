2e15b71bd0310cc396cb428809294c936a0a38f9
ThanGrove <than@virginia.edu>
2015-10-16 11:50:57 -0400

    Updates to fix the image grid except for the spacing between rows.

--- js/contrib/grid.js
+++ js/contrib/grid.js
@@ -1,3 +1,47 @@
+/*
+* debouncedresize: special jQuery event that happens once after a window resize
+*
+* latest version and complete README available on Github:
+* https://github.com/louisremi/jquery-smartresize/blob/master/jquery.debouncedresize.js
+*
+* Copyright 2011 @louis_remi
+* Licensed under the MIT license.
+*/
+
+(function ($) {
+var $event = $.event,
+$special,
+resizeTimeout;
+
+$special = $event.special.debouncedresize = {
+	setup: function() {
+		$( this ).on( "resize", $special.handler );
+	},
+	teardown: function() {
+		$( this ).off( "resize", $special.handler );
+	},
+	handler: function( event, execAsap ) {
+		// Save the context
+		var context = this,
+			args = arguments,
+			dispatch = function() {
+				// set correct event type
+				event.type = "debouncedresize";
+				$event.dispatch.apply( context, args );
+			};
+
+		if ( resizeTimeout ) {
+			clearTimeout( resizeTimeout );
+		}
+
+		execAsap ?
+			dispatch() :
+			resizeTimeout = setTimeout( dispatch, $special.threshold );
+	},
+	threshold: 250
+};
+
+}) (jQuery);
 
 var Grid = (function($) {
 	
@@ -52,6 +96,8 @@ var Grid = (function($) {
 		} );
 		*/
 		imagesLoaded($grid, $.proxy(function() {
+			var res = $("#og-grid").rowGrid({itemSelector: ".item", minMargin: 10, maxMargin: 10, firstItemClass: "first-item"});
+			console.log("result of rowGrid", res);
 			// save item´s size and offset
 			saveItemInfo( true );
 			// get window´s size
--- js/shanti-main-images.js
+++ js/shanti-main-images.js
@@ -3,14 +3,7 @@
 	    attach: function (context, settings) {
 			if(context == document) {
 				if ($("#og-grid").length > 0) {
-					$("#og-grid").rowGrid({itemSelector: ".item", minMargin: 10, maxMargin: 10, firstItemClass: "first-item"});
-					setTimeout( $.proxy(function() {
-						$('#og-grid items').attr('style', '');
-						$('#og-grid items img').attr('width', '');
-						Grid.init();
-						var itemHeight = jQuery(this).closest(".item").height();	 
-						$("#og-grid .item > a").children("img").attr("height", itemHeight );
-					}, this), 1000);
+					Grid.init();
 				}
 			}
 	    }
