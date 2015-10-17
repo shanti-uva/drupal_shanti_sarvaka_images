4a0261b6f1e5a0aa25118c70b0e071044b966638
dukarla <artesano@vpn-user-1-33.itc.Virginia.EDU>
2015-10-17 15:24:04 -0500

    testing thumbnails

--- js/shanti-main-images.js
+++ js/shanti-main-images.js
@@ -13,7 +13,7 @@
 	    attach: function (context, settings) {
 			if(context == document) {
 
-					var itemHeight = $(this).closest(".item").height();	
+					var itemHeight = jQuery(this).closest(".item").height();	
 
 					$(".og-grid > .item:not(.expanded)").next("a").click(function(e) {						
 						$(".og-grid > .item:not(.expanded)").next("a").css("height", itemHeight );
