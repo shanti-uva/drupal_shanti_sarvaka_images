a1c1b53e5e12f01bfd1089a07a6cf45576b23fce
dukarla <artesano@vpn-user-1-33.itc.Virginia.EDU>
2015-10-17 18:30:21 -0500

    testing thumbnails

--- js/shanti-main-images.js
+++ js/shanti-main-images.js
@@ -14,7 +14,7 @@
 			if(context == document) {
 
 					
-					$(".og-grid > .item > a").parent(".item:not(.expanded)").click(function(event) {						
+					$(".og-grid > .item > a").click(function(e) {						
 						var itemHeight = $(this).closest(".item").height();	
 						$(this).css("height", itemHeight );
 					});				
