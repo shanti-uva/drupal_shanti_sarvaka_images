6c76a69051e5d9d2c75c8424ff2b14fdf926a37e
ThanGrove <than@virginia.edu>
2015-10-21 17:02:29 -0400

    Adding code to hide or show admin menu based on mouse position

--- js/shanti-main-images.js
+++ js/shanti-main-images.js
@@ -5,6 +5,23 @@
 				if ($("#og-grid").length > 0) {
 					Grid.init();
 				}
+				setTimeout(function() { $('#admin-menu').slideUp(); $('body').removeClass('admin-menu'); }, 1000);
+				$('body').mousemove(function(event) { 
+					var x = event.pageX;
+					var y = event.pageY; 
+					if (y < 50 && x < 200) { 
+						if ($('#admin-menu').is(":hidden")) {
+							console.log("showing admin menu");
+							$('#admin-menu').slideDown(); 
+							$('body').addClass('admin-menu');
+						}
+					} else if (y > 100) { 
+						if ($('#admin-menu').is(":visible")) {
+							console.log("hiding admin menu");
+							setTimeout(function() { $('#admin-menu').slideUp(); $('body').removeClass('admin-menu'); }, 1500);
+						} 
+					} 
+				});
 			}
 	    }
 	};
