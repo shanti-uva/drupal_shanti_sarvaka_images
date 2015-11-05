6270126856f713c5c61c8f1172e3dbf92c19bb59
dukarla <artesano@vpn-user-1-105.itc.Virginia.EDU>
2015-11-04 18:17:46 -0600

    testing centering dropdown details

--- js/shanti-main-images.js
+++ js/shanti-main-images.js
@@ -27,24 +27,28 @@
 	};
 	
 	/*
-	 * popupImageCenter: jQuery extension function called in grid.js when opening popup. Positions image and lightbox link centered vertically
+	 * popupImageCenter: jQuery extension function called in grid.js when opening popup. Positions image and lightbox link centered vertically  */
 	 
     $.fn.popupImageCentering = function() {
 		return this.each(function() {
 			// Adjust top margin
+			/*
 			var 	wrapper = $(this).parents('.og-img-wrapper'), // get wrapper
 				 	imght = $(this).height(),
 					cnthgt = $(this).parents('.og-fullimg').height(),
 					tmarg = (cnthgt > imght) ? -imght / 2 : -cnthgt / 2;
 					
 			wrapper.css("margin-top",  tmarg  + "px" );
+			*/
 			//console.log("tmarg: " + tmarg);
 			
 			// Adjust left margin
+			/*
 			var 	imgwdt = $(this).width(),
 					cntwdt = $(this).parents('.og-fullimg').width(),
 					lmarg = (cntwdt > imgwdt) ? -imgwdt / 2 : -cntwdt / 2;
 			wrapper.css("margin-left",  lmarg  + "px" );
+			*/
 			//console.log("imgwdt: " + imgwdt);
 			//console.log("cntwdt: " + cntwdt);
 			//console.log("lmarg: " + lmarg);*
@@ -61,7 +65,7 @@
 			});
 	   };
 
-	   */
+	  
 
 
 }) (jQuery);
