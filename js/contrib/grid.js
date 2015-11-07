e95a44bb465774b1b6d32426542defc4a335c0b4
dukarla <artesano@vpn-user-1-238.itc.Virginia.EDU>
2015-11-07 14:02:27 -0600

    testing dropdown layout - restore img-wrapper

--- js/contrib/grid.js
+++ js/contrib/grid.js
@@ -352,8 +352,8 @@ var Grid = (function($) {
 			var self = this;
 			
 			// remove the current image in the preview
-			if( typeof self.$largeImgLink != 'undefined' ) {
-				self.$largeImgLink.remove();
+			if( typeof self.$largeImg != 'undefined' ) {
+				self.$largeImg.remove();
 			}
 
 			// preload large image and add it to the preview
@@ -365,13 +365,13 @@ var Grid = (function($) {
 					if( $img.attr( 'src' ) === self.$item.children('a').data( 'largesrc' ) ) {
 						//self.$loading.hide();
 						self.$fullimage.find( 'img' ).remove();
-						self.$fullimage.find('.lightbox-img-link').remove();
-						// self.$largeImgDiv = $('<div class="og-img-wrapper"></div>');
-						self.$largeImgLink = $( '<a href="#" class="lightbox-img-link"></a>' );
-						self.$lighboxLink = $( '<a href="#" class="lightbox-link btn-lightbox"><span class="icon fa-expand"></span></a>' );
-						self.$largeImgLink.append( self.$lighboxLink, $img.fadeIn( 100 ) );
-						// self.$largeImgLink.append( $img.fadeIn( 100 ) );
-						self.$fullimage.append( self.$largeImgLink );
+						self.$fullimage.find('.og-img-wrapper').remove();
+						self.$largeImgDiv = $('<div class="og-img-wrapper"></div>');
+						self.$largeImg = $( '<a href="#" class="lightbox-img-link"></a>' );
+						var span = $( '<a href="#" class="lightbox-link btn-lightbox"><span class="icon fa-expand"></span></a>' );
+						self.$largeImgDiv.append(span, self.$largeImg);
+						self.$largeImg.append($img.fadeIn( 100 ));
+						self.$fullimage.append( self.$largeImgDiv );
 						// Find the lightbox icon and enable click to initiate gallery
 						$("a.lightbox-link, a.lightbox-img-link").click(function(event) {
 							event.preventDefault();
@@ -385,7 +385,7 @@ var Grid = (function($) {
 							//console.log("Index new: " + iind);
 						});
 						setTimeout(function() {
-							jQuery(".lightbox-img-link img").popupImageCentering();
+							// jQuery(".og-img-wrapper img").popupImageCentering();
 						}, 300);
 					}
 				}).attr( 'src', eldata.largesrc );	
