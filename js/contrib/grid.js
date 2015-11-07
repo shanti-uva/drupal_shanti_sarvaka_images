b24d1deac23deaa0d55e87817f824ec191327cab
dukarla <artesano@vpn-user-1-151.itc.Virginia.EDU>
2015-11-06 20:24:25 -0600

    testing ligthbox link in anchor

--- js/contrib/grid.js
+++ js/contrib/grid.js
@@ -290,12 +290,11 @@ var Grid = (function($) {
 			this.$loading = $( '<div class="og-loading"></div>' );
 			this.$fullimage = $( '<div class="og-fullimg"></div>' ).append( this.$loading );
 			this.$closePreview = $( '<span class="og-close"></span>' );
-			this.$lightboxLink = $( '<a href="#" class="lightbox-link btn-lightbox"><span class="icon fa-expand"></span></a>' );
 			
 			this.$nextPreview = $( '<span class="next og-nav-arrow"><span class="icon"></span></span>' );
 			this.$prevPreview = $( '<span class="prev og-nav-arrow"><span class="icon"></span></span>' );
 
-			this.$previewInner = $( '<div class="og-expander-inner"></div>' ).append( this.$lightboxLink, this.$closePreview, this.$nextPreview, this.$prevPreview, this.$fullimage, this.$details );
+			this.$previewInner = $( '<div class="og-expander-inner"></div>' ).append( this.$closePreview, this.$nextPreview, this.$prevPreview, this.$fullimage, this.$details );
 			this.$previewEl = $( '<div class="og-expander"></div>' ).append( this.$previewInner );
 			// append preview element to the item
 			this.$item.append( this.getEl() );
@@ -369,12 +368,13 @@ var Grid = (function($) {
 						self.$fullimage.find('.og-img-wrapper').remove();
 						self.$largeImgDiv = $('<div class="og-img-wrapper"></div>');
 						self.$largeImg = $( '<a href="#" class="lightbox-img-link"></a>' );
-						//var span = $( '<a href="#" class="lightbox-link btn-lightbox"><span class="icon fa-expand"></span></a>' );
-						self.$largeImgDiv.append(self.$largeImg);
+						var span = $( '<a href="#" class="lightbox-link btn-lightbox"><span class="icon fa-expand"></span></a>' );
+						self.$largeImgDiv.append(span, self.$largeImg);
 						self.$largeImg.append($img.fadeIn( 100 ));
 						self.$fullimage.append( self.$largeImgDiv );
 						// Find the lightbox icon and enable click to initiate gallery
-						self.$fullimage.find("a.lightbox-link, a.lightbox-img-link").eq(0).click(function() {
+						$("a.lightbox-link, a.lightbox-img-link").click(function(event) {
+							event.preventDefault();
        						var pswpElement = document.querySelectorAll('.pswp')[0];
        						var iind = self.$item.prevAll().length;
 							var options = { index: iind };
@@ -385,7 +385,7 @@ var Grid = (function($) {
 							//console.log("Index new: " + iind);
 						});
 						setTimeout(function() {
-							// jQuery(".og-img-wrapper img").popupImageCentering();
+							jQuery(".og-img-wrapper img").popupImageCentering();
 						}, 300);
 					}
 				}).attr( 'src', eldata.largesrc );	
